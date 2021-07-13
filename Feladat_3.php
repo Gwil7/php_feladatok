660609b7c4fedeb141c2cba203ccdd4ec53b5c72
Grúz Melinda <77782511+Gwil7@users.noreply.github.com>
2021-07-13 12:01:52 +0200

    Add files via upload

---
+++ Feladat_3.php
@@ -0,0 +1,9 @@
+<?php
+$date_now = date("Y.m.d");
+echo "A dátum: " . $date_now;
+
+
+
+
+?>
+Kérem adjon meg egy dátumot: <input type="date" name="requested_date" value=$date_now>;
---
+++ PHP feladatok.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <!---- Feladat 3 -->  
+        <?php  
+            include("Feladat_3.php");
+        ?>
+         Kérem adjon meg egy dátumot: <input type="date" name="requested_date">;
+    </body>
+</html>
